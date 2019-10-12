33bb57bedf07196164847278137fd53ea76662f7
Gary <547092829@qq.com>
2019-10-12 16:22:58 +0800

    根据原有项目迁移至py3版本

--- WeatherSpider/WeatherSpider/items.py
+++ WeatherSpider/WeatherSpider/items.py
@@ -10,5 +10,21 @@ import scrapy
 
 class WeatherspiderItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    # 地区名
+    area = scrapy.Field()
+    # 采集来源的url
+    url = scrapy.Field()
+    # 采集时间
+    timestamp = scrapy.Field()
+    # 日期
+    date = scrapy.Field()
+    # 最高气温
+    max_t = scrapy.Field()
+    # 最低气温
+    min_t = scrapy.Field()
+    # 天气
+    weather = scrapy.Field()
+    # 风向
+    wind_direction = scrapy.Field()
+    # 风力
+    wind_power = scrapy.Field()
--- WeatherSpider/WeatherSpider/pipelines.py
+++ WeatherSpider/WeatherSpider/pipelines.py
@@ -5,7 +5,20 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
+import json
+
 
 class WeatherspiderPipeline(object):
+    def open_spider(self, spider):
+        self.file = open('weather.json', 'w')
+
     def process_item(self, item, spider):
+
+        data = json.dumps(dict(item), ensure_ascii=False, indent=2)
+
+        self.file.write(data)
+
         return item
+
+    def close_spider(self, spider):
+        self.file.close()
